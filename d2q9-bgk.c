214138a10ce0406e717f930ec5b487d15df7bc72
georgeherbert <george.herbert@icloud.com>
2022-02-14 11:14:08 +0000

    Missed out some powers of 2

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -230,18 +230,22 @@ float timestep(const t_param params, t_speed* restrict cells, t_speed* restrict 
 
   __assume(params.ny % 2 == 0);
   __assume(params.ny % 4 == 0);
+  __assume(params.nx % 8 == 0);
   __assume(params.ny % 16 == 0);
   __assume(params.ny % 32 == 0);
   __assume(params.ny % 64 == 0);
   __assume(params.ny % 128 == 0);
+  __assume(params.ny % 256 == 0);
   __assume(params.ny % 512 == 0);
   __assume(params.ny % 1024 == 0);
   __assume(params.nx % 2 == 0);
   __assume(params.nx % 4 == 0);
+  __assume(params.nx % 8 == 0);
   __assume(params.nx % 16 == 0);
   __assume(params.nx % 32 == 0);
   __assume(params.nx % 64 == 0);
   __assume(params.nx % 128 == 0);
+  __assume(params.nx % 256 == 0);
   __assume(params.nx % 512 == 0);
   __assume(params.nx % 1024 == 0);
   __assume_aligned(cells, 64);

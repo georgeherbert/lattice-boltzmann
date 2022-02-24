97bbd77cad1043e9158b6a3ca0373c520604f903
georgeherbert <george.herbert@icloud.com>
2022-02-24 17:11:15 +0000

    Loop fusion version of d2q9-bgk

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -69,7 +69,6 @@ typedef struct {
   float density; /* density per link */
   float accel; /* density redistribution */
   float omega; /* relaxation parameter */
-  float num_non_obstacles_r;
 } t_param;
 
 /* struct to hold the 'speed' values */
@@ -211,12 +210,11 @@ int accelerate_flow(const t_param params, t_speed* cells, int* obstacles) {
 }
 
 float timestep(const t_param params, t_speed* cells, t_speed* cells_new, int* obstacles) {
-  const float c_sq_r = 3.f;
-  const float two_c_sq_r = 3.f / 2.f;
-  const float two_c_sq_sq_r = 9.f / 2.f;
-  const float w0 = 4.f / 9.f; /* weighting factor */
-  const float w1 = 1.f / 9.f; /* weighting factor */
+  const float c_sq = 1.f / 3.f; /* square of speed of sound */
+  const float w0 = 4.f / 9.f;  /* weighting factor */
+  const float w1 = 1.f / 9.f;  /* weighting factor */
   const float w2 = 1.f / 36.f; /* weighting factor */
+  int    tot_cells = 0;
   float tot_u = 0.f; /* accumulated magnitudes of velocity for each cell */
 
   /* loop over the cells in the grid */
@@ -258,18 +256,48 @@ float timestep(const t_param params, t_speed* cells, t_speed* cells_new, int* ob
         /* velocity squared */
         const float u_sq = u_x * u_x + u_y * u_y;
         
+        /* directional velocity components */
+        float u[NSPEEDS];
+        u[1] =   u_x;        /* east */
+        u[2] =         u_y;  /* north */
+        u[3] = - u_x;        /* west */
+        u[4] =       - u_y;  /* south */
+        u[5] =   u_x + u_y;  /* north-east */
+        u[6] = - u_x + u_y;  /* north-west */
+        u[7] = - u_x - u_y;  /* south-west */
+        u[8] =   u_x - u_y;  /* south-east */
+
         /* equilibrium densities */
-        const float d_equ[NSPEEDS] = {
-          w0 * local_density * (1.f - u_sq * two_c_sq_r),
-          w1 * local_density * (1.f + u_x * c_sq_r + (u_x * u_x) * two_c_sq_sq_r - u_sq * two_c_sq_r),
-          w1 * local_density * (1.f + u_y * c_sq_r + (u_y * u_y) * two_c_sq_sq_r - u_sq * two_c_sq_r),
-          w1 * local_density * (1.f + -u_x * c_sq_r + (-u_x * -u_x) * two_c_sq_sq_r - u_sq * two_c_sq_r),
-          w1 * local_density * (1.f + -u_y * c_sq_r + (-u_y * -u_y) * two_c_sq_sq_r - u_sq * two_c_sq_r),
-          w2 * local_density * (1.f + (u_x + u_y) * c_sq_r + ((u_x + u_y) * (u_x + u_y)) * two_c_sq_sq_r - u_sq * two_c_sq_r),
-          w2 * local_density * (1.f + (-u_x + u_y) * c_sq_r + ((-u_x + u_y) * (-u_x + u_y)) * two_c_sq_sq_r - u_sq * two_c_sq_r),
-          w2 * local_density * (1.f + (-u_x - u_y) * c_sq_r + ((-u_x - u_y) * (-u_x - u_y)) * two_c_sq_sq_r - u_sq * two_c_sq_r),
-          w2 * local_density * (1.f + (u_x - u_y) * c_sq_r + ((u_x - u_y) * (u_x - u_y)) * two_c_sq_sq_r - u_sq * two_c_sq_r)
-        };
+        float d_equ[NSPEEDS];
+        /* zero velocity density: weight w0 */
+        d_equ[0] = w0 * local_density
+                   * (1.f - u_sq / (2.f * c_sq));
+        /* axis speeds: weight w1 */
+        d_equ[1] = w1 * local_density * (1.f + u[1] / c_sq
+                                         + (u[1] * u[1]) / (2.f * c_sq * c_sq)
+                                         - u_sq / (2.f * c_sq));
+        d_equ[2] = w1 * local_density * (1.f + u[2] / c_sq
+                                         + (u[2] * u[2]) / (2.f * c_sq * c_sq)
+                                         - u_sq / (2.f * c_sq));
+        d_equ[3] = w1 * local_density * (1.f + u[3] / c_sq
+                                         + (u[3] * u[3]) / (2.f * c_sq * c_sq)
+                                         - u_sq / (2.f * c_sq));
+        d_equ[4] = w1 * local_density * (1.f + u[4] / c_sq
+                                         + (u[4] * u[4]) / (2.f * c_sq * c_sq)
+                                         - u_sq / (2.f * c_sq));
+        /* diagonal speeds: weight w2 */
+        d_equ[5] = w2 * local_density * (1.f + u[5] / c_sq
+                                         + (u[5] * u[5]) / (2.f * c_sq * c_sq)
+                                         - u_sq / (2.f * c_sq));
+        d_equ[6] = w2 * local_density * (1.f + u[6] / c_sq
+                                         + (u[6] * u[6]) / (2.f * c_sq * c_sq)
+                                         - u_sq / (2.f * c_sq));
+        d_equ[7] = w2 * local_density * (1.f + u[7] / c_sq
+                                         + (u[7] * u[7]) / (2.f * c_sq * c_sq)
+                                         - u_sq / (2.f * c_sq));
+        d_equ[8] = w2 * local_density * (1.f + u[8] / c_sq
+                                         + (u[8] * u[8]) / (2.f * c_sq * c_sq)
+                                         - u_sq / (2.f * c_sq));
 
         /* relaxation step */
         cells_new[ii + jj * params.nx].speeds[0] = cells[ii + jj * params.nx].speeds[0] + params.omega * (d_equ[0] - cells[ii + jj * params.nx].speeds[0]);
@@ -283,10 +311,11 @@ float timestep(const t_param params, t_speed* cells, t_speed* cells_new, int* ob
         cells_new[ii + jj * params.nx].speeds[8] = cells[x_w + y_n * params.nx].speeds[8] + params.omega * (d_equ[8] - cells[x_w + y_n * params.nx].speeds[8]);
         
         tot_u += sqrtf(u_sq);
+        ++tot_cells;
       }
     }
   }
-  return tot_u * params.num_non_obstacles_r;
+  return tot_u / (float)tot_cells;
 }
 
 float av_velocity(const t_param params, t_speed* cells, int* obstacles) {
@@ -433,7 +462,6 @@ int initialise(const char* paramfile, const char* obstaclefile, t_param* params,
     die(message, __LINE__, __FILE__);
   }
 
-  int num_obstacles = 0;
   /* read-in the blocked cells list */
   while ((retval = fscanf(fp, "%d %d %d\n", &xx, &yy, &blocked)) != EOF) {
     /* some checks */
@@ -444,9 +472,7 @@ int initialise(const char* paramfile, const char* obstaclefile, t_param* params,
 
     /* assign to array */
     (*obstacles_ptr)[xx + yy*params->nx] = blocked;
-    ++num_obstacles;
   }
-  params->num_non_obstacles_r = 1.f / (params->nx * params->ny - num_obstacles);
 
   /* and close the file */
   fclose(fp);

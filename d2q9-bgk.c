eb2112a1cd1bbebd79150baded4e71c37a4f4136
georgeherbert <george.herbert@icloud.com>
2022-03-28 21:59:08 +0100

    Removed a useless assignment (I think)

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -275,7 +275,7 @@ float timestep(const t_param params, const t_speed* cells, t_speed* cells_new, c
     float tot_u = 0.f; /* accumulated magnitudes of velocity for each cell */
 
     /* loop over the cells in the grid */
-    // #pragma omp parallel for schedule(static), reduction(+:tot_u)
+    #pragma omp parallel for schedule(static), reduction(+:tot_u)
     for (int jj = 1; jj < params.num_rows + 1; jj++) {
         /* determine indices of north and south axis-direction neighbours 
         ** respecting periodic boundary conditions (wrap around) */
@@ -402,7 +402,6 @@ void halo_exchange(const t_param* params, t_speed* cells, float* send_row_buffer
 
 void collate(const t_param* params, t_speed* cells, t_speed* cells_complete, float* send_section_buffer, float* receive_section_buffer, float* av_vels, float* av_vels_buffer, const int tag, MPI_Status* status) {
     if (params->rank == MASTER) {
-        cells->speeds_0[0] = 0.f;
         for (int jj = 0; jj < params->num_rows; jj++) {
             for (int ii = 0; ii < params->nx; ii++) {
                 cells_complete->speeds_0[ii + jj * params->nx] = cells->speeds_0[ii + (jj + 1) * params->nx];
@@ -604,7 +603,7 @@ int initialise(const char* paramfile, const char* obstaclefile, t_param* params,
     float w1 = params->density / 9.f;
     float w2 = params->density / 36.f;
 
-    // #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(static)
     for (int jj = 0; jj < params->num_rows + 2; jj++) {
         for (int ii = 0; ii < params->nx; ii++) {
             /* centre */
@@ -623,7 +622,7 @@ int initialise(const char* paramfile, const char* obstaclefile, t_param* params,
     }
 
     /* first set all cells in obstacle array to zero */
-    // #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(static)
     for (int jj = 0; jj < params->num_rows + 2; jj++) {
         for (int ii = 0; ii < params->nx; ii++) {
             (*obstacles_ptr)[ii + jj * params->nx] = 0;

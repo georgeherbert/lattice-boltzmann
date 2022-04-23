ba6310a5725ad98ba56b00759875318e84b88b22
georgeherbert <george.herbert@icloud.com>
2022-04-23 12:30:40 +0100

    Created a grid

---
+++ generate_obstacles.py
@@ -0,0 +1,21 @@
+WIDTH = 8192
+HEIGHT = 8192
+
+f = open(f"obstacles_{WIDTH}x{HEIGHT}.dat", "w")
+
+for i in range(WIDTH):
+    f.write(f"{i} 0 1\n")
+for i in range(WIDTH):
+    f.write(f"{i} {HEIGHT - 1} 1\n")
+for i in range(HEIGHT):
+    f.write(f"{WIDTH - 1} {i} 1\n")
+for i in range(HEIGHT):
+    f.write(f"0 {i} 1\n")
+
+## OPTIONAL
+for i in range(HEIGHT):
+    f.write(f"{WIDTH // 4} {i} 1\n")
+for i in range(HEIGHT):
+    f.write(f"{(WIDTH // 4) * 2} {i} 1\n")
+
+f.close()

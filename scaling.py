0fac8d391ebcbe77e0185e07dcfb16bb82a5d005
georgeherbert <george.herbert@icloud.com>
2022-04-27 17:16:57 +0100

    Added new scaling

--- scaling.py
+++ scaling.py
@@ -3,7 +3,7 @@ def get_times_lines():
     small_2_times_lines = []
     medium_times_lines = []
     big_times_lines = []
-    for thread in range(1, 29):
+    for thread in range(1, 113):
         with open(f"d2q9-bgk_procs/d2q9-bgk_{thread}.out") as file:
             content = file.readlines()
             small_times_lines.append(content[11].strip())
@@ -23,8 +23,8 @@ def convert_to_float(times_lines):
 def print_scalings(times):
     smallest_time = times[0]
     for i, time in enumerate(times, start = 1):
-        # print(f"({i}, {smallest_time / time})")
-        print(smallest_time / time)
+        print(f"({i}, {smallest_time / time})")
+        # print(smallest_time / time)
         # print(time)
 
 def main():
@@ -33,13 +33,13 @@ def main():
     small_2_times = convert_to_float(small_2_times_lines)
     medium_times = convert_to_float(medium_times_lines)
     big_times = convert_to_float(big_times_lines)
-    print_scalings(small_times)
-    print("")
-    print_scalings(small_2_times)
-    print("")
-    print_scalings(medium_times)
-    print("")
-    print_scalings(big_times)
+    # print_scalings(small_times)
+    # print("")
+    # print_scalings(small_2_times)
+    # print("")
+    # print_scalings(medium_times)
+    # print("")
+    # print_scalings(big_times)
 
 
 if __name__ == "__main__":
